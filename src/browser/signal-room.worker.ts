29542eb5826af8a4e16fb628bafc1bb4894ddad8
jacklehamster <jacklehamster@gmail.com>
2026-01-03 16:18:19 -0800

    Reapply "include host/room"
    This reverts commit c8f064d7f534f5db992cf58e35e87231ba511b76.

--- src/browser/signal-room.worker.ts
+++ src/browser/signal-room.worker.ts
@@ -26,7 +26,14 @@ export type WorkerCommand<T extends string = string, P = any> =
       autoRejoin: boolean;
     }
   | { cmd: "exit" }
-  | { cmd: "send"; toUserId: string; type: T; payload: P };
+  | {
+      cmd: "send";
+      host: string;
+      room: string;
+      toUserId: string;
+      type: T;
+      payload: P;
+    };
 
 let exitRoom: (() => void) | null = null;
 
@@ -67,19 +74,23 @@ self.addEventListener("message", (e: MessageEvent<WorkerCommand>) => {
       },
       onPeerJoined: (users: IPeer[]) => {
         // Save the ability to send to this peer
-        users.forEach(({ userId, receive }) => peerSend.set(userId, receive));
+        users.forEach(({ userId, receive }) =>
+          peerSend.set(`${msg.host}/${msg.room}/${userId}`, receive)
+        );
         emit({
           kind: "peer-joined",
           users: users.map(({ userId }) => ({ userId })),
         });
       },
       onPeerLeft: (users: { userId: string }[]) => {
-        users.forEach(({ userId }) => peerSend.delete(userId));
+        users.forEach(({ userId }) =>
+          peerSend.delete(`${msg.host}/${msg.room}/${userId}`)
+        );
         emit({ kind: "peer-left", users });
       },
       onMessage: (type: any, payload: any, from: IPeer) => {
         // We can also learn peerSend via onMessage in case join events vary
-        peerSend.set(from.userId, from.receive);
+        peerSend.set(`${msg.host}/${msg.room}/${from.userId}`, from.receive);
         emit({
           kind: "message",
           type,
@@ -94,7 +105,7 @@ self.addEventListener("message", (e: MessageEvent<WorkerCommand>) => {
   }
 
   if (msg.cmd === "send") {
-    const sendFn = peerSend.get(msg.toUserId);
+    const sendFn = peerSend.get(`${msg.host}/${msg.room}/${msg.toUserId}`);
     if (sendFn) sendFn(msg.type, msg.payload);
     return;
   }
